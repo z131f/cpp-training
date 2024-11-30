7a3bbfd76dc3d427ae75b51bf46d9b549973c54f
z131f <zjw131f1fc@outlook.com>
2024-11-30 19:53:33 +0800

    Extract move, turnleft and turnright

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -2,6 +2,10 @@
 
 namespace adas
 {
+
+const std::unordered_map<char, int> direction_map = {{'N', 0}, {'E', 1}, {'S', 2}, {'W', 3}};
+const std::unordered_map<int, char> reverse_direction_map = {{0, 'N'}, {1, 'E'}, {2, 'S'}, {3, 'W'}};
+
 Executor* Executor::NewExecutor(const Pose& pose) noexcept
 {
     return new (std::nothrow) ExecutorImpl(pose);
@@ -13,34 +17,43 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : pose(pose)
 
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
-    const std::unordered_map<char, int> direction_map = {
-        {'N', 0},
-        {'E', 1},
-        {'S', 2},
-        {'W', 3}
-    };
-
-    const std::unordered_map<int, char> reverse_direction_map = {
-        {0, 'N'},
-        {1, 'E'},
-        {2, 'S'},
-        {3, 'W'}
-    };
-
     for (const auto cmd : commands) {
         if (cmd == 'M') {
-            switch (pose.heading) {
-                case 'N': ++pose.y; break;
-                case 'E': ++pose.x; break;
-                case 'S': --pose.y; break;
-                case 'W': --pose.x; break;
-            }
+            Move();
         } else if (cmd == 'L') {
-            pose.heading = reverse_direction_map.at((direction_map.at(pose.heading) + 3) % 4);  // Turn left
+            TurnLeft();
         } else if (cmd == 'R') {
-            pose.heading = reverse_direction_map.at((direction_map.at(pose.heading) + 1) % 4);  // Turn right
+            TurnRight();
         }
     }
+}
+
+void ExecutorImpl::Move() noexcept
+{
+    switch (pose.heading) {
+    case 'N':
+        ++pose.y;
+        break;
+    case 'E':
+        ++pose.x;
+        break;
+    case 'S':
+        --pose.y;
+        break;
+    case 'W':
+        --pose.x;
+        break;
+    }
+}
+
+void ExecutorImpl::TurnLeft() noexcept
+{
+    pose.heading = reverse_direction_map.at((direction_map.at(pose.heading) + 3) % 4);  // Turn left
+}
+
+void ExecutorImpl::TurnRight() noexcept
+{
+    pose.heading = reverse_direction_map.at((direction_map.at(pose.heading) + 1) % 4);  // Turn right
 }
 
 Pose ExecutorImpl::Query() const noexcept
--- src/ExecutorImpl.hpp
+++ src/ExecutorImpl.hpp
@@ -17,5 +17,8 @@ public:
 
 private:
     Pose pose;
+    void Move(void) noexcept;
+    void TurnLeft(void) noexcept;
+    void TurnRight(void) noexcept;
 };
 }  // namespace adas
