d96b2b44daab6ea867de8dbc1095f307481b6e1f
z131f <zjw131f1fc@outlook.com>
2024-12-07 19:27:26 +0800

    Add test cases for back and fast command

---
+++ tests/ExecutorBackFastTest.cpp
@@ -0,0 +1,108 @@
+#include <gtest/gtest.h>
+
+#include "Executor.hpp"
+#include "PoseEq.hpp"
+
+namespace adas
+{
+
+TEST(ExecutorBackFastTest, should_return_x_minus_1_given_status_is_B_command_is_M_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BM");
+    // then
+    const Pose target{-1, 0, 'E'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorBackFastTest, should_return_S_given_status_is_B_command_is_L_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BL");
+    // then
+    const Pose target{0, 0, 'S'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorBackFastTest, should_return_N_given_status_is_B_given_command_is_R_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BR");
+    // then
+    const Pose target{0, 0, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorBackFastTest, should_return_x_minus_2_given_command_is_BFM_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BFM");
+    // then
+    const Pose target{-2, 0, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorBackFastTest, should_return_S_and_x_minus_1_given_command_is_BFL_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BFL");
+    // then
+    const Pose target{-1, 0, 'S'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorBackFastTest, should_return_N_and_x_minus_1_given_command_is_BFR_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BFR");
+    // then
+    const Pose target{-1, 0, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorBackFastTest, should_return_x_plus_1_given_command_is_BBM_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BBM");
+    // then
+    const Pose target{1, 0, 'E'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorBackFastTest, should_return_N_given_command_is_BBL_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BBL");
+    // then
+    const Pose target{0, 0, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorBackFastTest, should_return_S_given_command_is_BBR_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when
+    executor->Execute("BBR");
+    // then
+    const Pose target{0, 0, 'S'};
+    ASSERT_EQ(target, executor->Query());
+}
+
+}  // namespace adas
