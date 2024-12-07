d3bbb8c9ad58ec315911058206e06d3139de4c97
z131f <zjw131f1fc@outlook.com>
2024-12-07 19:46:50 +0800

    Add command map

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -1,6 +1,7 @@
 #include "ExecutorImpl.hpp"
 
 #include <memory>
+#include <unordered_map>
 
 #include "Command.hpp"
 
@@ -18,19 +19,17 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : poseHandler(pose)
 
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
+    std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
+    cmderMap.emplace('M', std::make_unique<MoveCommand>());
+    cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
+    cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
+    cmderMap.emplace('F', std::make_unique<FastCommand>());
+
     for (const auto cmd : commands) {
-        std::unique_ptr<ICommand> cmder;
-        if (cmd == 'M') {
-            cmder = std::make_unique<MoveCommand>();
-        } else if (cmd == 'L') {
-            cmder = std::make_unique<TurnLeftCommand>();
-        } else if (cmd == 'R') {
-            cmder = std::make_unique<TurnRightCommand>();
-        } else if (cmd == 'F') {
-            cmder = std::make_unique<FastCommand>();
-        }
-        if (cmder) {
-            cmder->DoOperate(poseHandler);
+        const auto it = cmderMap.find(cmd);
+
+        if (it != cmderMap.end()) {
+            it->second->DoOperate(poseHandler);
         }
     }
 }
